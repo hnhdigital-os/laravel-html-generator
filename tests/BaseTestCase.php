583e2dc8cf828195884d90b23332312a9b90c8f4
Rocco Howard <rocco@bluora.com.au>
2016-10-27 00:06:38 +1100

    Fixed testing

---
+++ tests/BaseTestCase.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Bluora\LaravelHtmlGenerator\Tests;
+
+use Mockery;
+use PHPUnit_Framework_TestCase;
+
+abstract class BaseTestCase extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Tear down test.
+     *
+     * @return void
+     */
+    protected function tearDown()
+    {
+        parent::tearDown();
+        Mockery::close();
+    }
+}
